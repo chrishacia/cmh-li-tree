da0a3ac84dfd46f45c22aa822e47d961285f5789
Christopher Hacia <global@chrishacia.com>
2013-04-02 12:52:31 -0400

    some tree level elements have no sub elements, so why toggle them open or close?

--- cmh-li-tree.js
+++ cmh-li-tree.js
@@ -10,17 +10,19 @@
 //NOTES:
 // Work up some better commenting..
 // Convert normal functions to class based
-var elemID = 'testID';
-function tree_collapsetree_openRowSubs(elemID)
+var theElem = '.litree';
+function tree_collapsetree_openRowSubs(theElem)
 {
-	$('ul#'+elemID).find('ul').addClass('tree_collapse').removeClass('tree_open');
+	$('ul'+theElem).find('ul').addClass('tree_collapse').removeClass('tree_open');
 }
 //finds any ul's with the class "tree_open" (rendered at time of load)
 //and tree_opens them for show.
-function zebraStyle(elemID)
+function zebraStyle(theElem)
 {
-	$('ul#'+elemID+' > li, ul#'+elemID+' ul > li:nth-child(odd)').addClass('li_odds');
-	$('ul#'+elemID+' > li, ul#'+elemID+' ul > li:nth-child(even)').addClass('li_evens');
+	//$('ul'+theElem+' > li, ul'+theElem+' ul > li:nth-child(odd)').addClass('li_odds');
+	//$('ul'+theElem+' > li, ul'+theElem+' ul > li:nth-child(even)').addClass('li_evens');
+	$('ul'+theElem+' li:nth-child(odd)').addClass('li_odds');
+	$('ul'+theElem+' li:nth-child(even)').addClass('li_evens');
 }
 function findtree_openRowSubs()
 {
@@ -29,24 +31,31 @@ function findtree_openRowSubs()
     .parentsUntil('.tree_subset', 'ul.tree_collapse')
     .removeClass('tree_collapse tree_open');
 }
-$('ul#'+elemID+' > li > h2, ul#'+elemID+' ul > li > h2').click(function()
+$('ul'+theElem+' > li > h2, ul'+theElem+' ul > li > h2').live('click',function()
 {
-	var $setEl = $(this).next('ul');
-	if($setEl.hasClass('tree_collapse'))
+	if(!$(this).hasClass('tree_no_toggle'))
 	{
-		$setEl.removeClass('tree_collapse');
-	}
-	else
-	{
-		$setEl.addClass('tree_collapse');
+		var $setEl = $(this).next('ul');
+		if($setEl.hasClass('tree_collapse'))
+		{
+			$setEl.removeClass('tree_collapse');//.addClass('tree_border');
+		}
+		else
+		{
+			$setEl.addClass('tree_collapse');//.removeClass('tree_border');
+		}
 	}
 });
-$('#tree_collapse_rows').click(function(){
-	tree_collapsetree_openRowSubs(elemID);
+$('#tree_collapse_rows').live('click',function(){
+	tree_collapsetree_openRowSubs(theElem);
 });
 
 $(document).ready(function()
 {
-	findtree_openRowSubs(elemID);
-	zebraStyle(elemID);
+	xm = new Array();
+	//alert(typeof xm)
+	findtree_openRowSubs(theElem);
+	zebraStyle(theElem);
 });
+
+//<ul class="tree_open"> to invoke forced open of any ul/li and its parents.
